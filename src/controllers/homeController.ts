bc09e2db81c15c9092847110c6dd62e5af89720b
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-09-13 23:39:39 -0300

    Tipos de consulta 1

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,29 +1,37 @@
 import { Request, Response } from "express";
 
+/** Tipos de consulta 1 */
+import { Op } from "sequelize";
+
 /** Para instanciar conexão com mysql */
 //import { sequelize } from "../connection/mysql";
 
 /** Para instanciar conexão com postgres */
-import { sequelize } from "../connection/postgres";
+//import { sequelize } from "../connection/postgres";
 
 import { Product } from "../models/Product";
 import { User } from "../models/User";
 
 export const home = async (req: Request, res: Response) => {
-
-  let users = await User.findAll();
+  /** Consultando dados do Model */
+  //let users = await User.findAll({});
 
   /** Para exibir todos os usuários no console */
   //console.log("USUÁRIOS: ", JSON.stringify(users));
 
+  /** Tipos de consulta 1 */
+  let users = await User.findAll({
+    //attributes: ['name', 'age']
+    //attributes: { exclude: ['id']}
+    //where: { name: 'leonardo'} // Filtragem simples
+    //where: { name: 'leonardo', age: '20' } // Filtragem E
+    where: {
+      //[Op.or]: [{ age: 24 }, { name: "Natan" }], // Filtragem "OU"
+      age: { [Op.or]: [24, 23] }, // Filtragem "OU"
+    },
+  });
 
-
-
-
-
-
-
-
+  /** Conectando ao banco de dados*/
   /*
   try {
     await sequelize.authenticate();
@@ -50,6 +58,6 @@ export const home = async (req: Request, res: Response) => {
     products: list,
     expensives: expensiveList,
     frasesDoDia: [],
-    users
+    users,
   });
 };
